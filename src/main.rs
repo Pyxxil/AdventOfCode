c70adbc073c2ac6a8b7cc3caf74bac136da77f86
Joshua Hill <josh@pyxxilated.studio>
2020-12-04 14:44:50 +1300

    Add Command Line Interface

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
+use structopt::StructOpt;
+
 #[macro_use]
 mod day;
 use day::{one::One, three::Three, two::Two, Day};
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "calendar", about = "An Advent of Code Calendar")]
+struct Opt {
+    /// The Days to display
+    #[structopt(short, long="day")]
+    days: Vec<u64>,
+}
+
 fn main() {
-    run!(One, Two, Three);
+    let mut opt = Opt::from_args();
+
+    if opt.days.is_empty() {
+        opt.days = vec![1, 2, 3];
+    }
+
+    opt.days.into_iter().for_each(|day|
+        match day {
+            1 => run!(One),
+            2 => run!(Two),
+            3 => run!(Three),
+            _ => eprintln!("That day doesn't exist in the calendar or hasn't passed yet!"),
+        }
+    );
 }
