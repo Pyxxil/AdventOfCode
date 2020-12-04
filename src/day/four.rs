526548148ac3f63773e6a75ef2f0904572918732
Joshua Hill <josh@pyxxilated.studio>
2020-12-04 20:39:11 +1300

    Add Documentation and Refactor Part Four

--- src/day/four.rs
+++ src/day/four.rs
@@ -4,66 +4,130 @@ use crate::day::Day;
 
 pub struct Four {}
 
+static EYE_COLOURS: [&str; 7] = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+static PASSPORT_KEYS: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+pub struct Passport {
+    hair_colour: String,
+    eye_colour: String,
+    birth_year: i32,
+    issue_year: i32,
+    height: i32,
+    system: String,
+    expiration_year: i32,
+    id: String,
+}
+
+impl Passport {
+    pub fn check(map: &HashMap<String, String>) -> bool {
+        PASSPORT_KEYS
+            .iter()
+            .all(|key| map.contains_key(&key.to_string()))
+    }
+
+    pub fn from(map: &HashMap<String, String>) -> Result<Self, ()> {
+        let birth_year = map.get("byr").ok_or(())?.parse::<i32>().or(Err(()))?;
+        let issue_year = map.get("iyr").ok_or(())?.parse::<i32>().or(Err(()))?;
+        let expiration_year = map.get("eyr").ok_or(())?.parse::<i32>().or(Err(()))?;
+        let height = map
+            .get("hgt")
+            .ok_or(())?
+            .chars()
+            .take_while(|ch| ch.is_digit(10))
+            .collect::<String>()
+            .parse::<i32>()
+            .or(Err(()))?;
+        let system = map
+            .get("hgt")
+            .ok_or(())?
+            .chars()
+            .skip_while(|ch| ch.is_digit(10))
+            .collect::<String>();
+        let hair_colour = map.get("hcl").ok_or(())?.clone();
+        let eye_colour = map.get("ecl").ok_or(())?.clone();
+        let passport_id = map.get("pid").ok_or(())?.clone();
+
+        Ok(Self {
+            hair_colour,
+            eye_colour,
+            birth_year,
+            issue_year,
+            height,
+            system,
+            expiration_year,
+            id: passport_id,
+        })
+    }
+
+    pub fn is_valid(&self) -> bool {
+        if self.birth_year < 1920 || self.birth_year > 2002 {
+            false
+        } else if self.issue_year < 2010 || self.issue_year > 2020 {
+            false
+        } else if self.expiration_year < 2020 || self.expiration_year > 2030 {
+            false
+        } else if self.hair_colour.len() != 7
+            || self.hair_colour.chars().next().unwrap_or(' ') != '#'
+            || self.hair_colour.chars().skip(1).any(|ch| !ch.is_digit(16))
+        {
+            false
+        } else if !EYE_COLOURS.iter().any(|c| c == &self.eye_colour) {
+            false
+        } else if self.id.len() != 9 {
+            false
+        } else if !((self.system == "cm" && self.height >= 150 && self.height <= 193)
+            || (self.system == "in" && self.height >= 59 && self.height <= 76))
+        {
+            false
+        } else {
+            true
+        }
+    }
+}
+
 impl Day for Four {
     type Input = Vec<HashMap<String, String>>;
     type Output = usize;
 
     ///
-    /// Task:
+    /// Task: Determine how many valid passports there are, where a
+    ///       valid passport contains the following:
+    ///        - byr (Birth Year)
+    ///        - iyr (Issue Year)
+    ///        - eyr (Expiration Year)
+    ///        - hgt (Height)
+    ///        - hcl (Hair Color)
+    ///        - ecl (Eye Color)
+    ///        - pid (Passport ID)
+    ///
+    ///       There is an optional field (cid: Country ID).
     ///
     fn part_one(passports: &Self::Input) -> Self::Output {
-        passports
-            .iter()
-            .filter(|passport| {
-                passport.contains_key("byr")
-                    && passport.contains_key("iyr")
-                    && passport.contains_key("eyr")
-                    && passport.contains_key("hgt")
-                    && passport.contains_key("hcl")
-                    && passport.contains_key("ecl")
-                    && passport.contains_key("pid")
-            })
-            .count()
+        // Invalid (in accordance with this parts rules) will have been
+        // filtered out before now
+        passports.len()
     }
 
     ///
-    /// Task:
+    /// Task: Determine how many passwords have the correct
+    ///       corresponding values to them as well:
+    ///        -byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    ///        -iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    ///        -eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    ///        -hgt (Height) - a number followed by either cm or in:
+    ///             If cm, the number must be at least 150 and at most 193.
+    ///             If in, the number must be at least 59 and at most 76.
+    ///        -hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    ///        -ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    ///        -pid (Passport ID) - a nine-digit number, including leading zeroes.
+    ///        -cid (Country ID) - ignored, missing or not.
     ///
     fn part_two(passports: &Self::Input) -> Self::Output {
         passports
             .iter()
-            .filter(|passport| {
-                let empty = String::new();
-                let zero = "0".to_string();
-                let birth_year = passport.get("byr").map_or_else(|| 0, |p| p.parse::<i32>().unwrap());
-                let issue_year = passport.get("iyr").map_or_else(|| 0, |p| p.parse::<i32>().unwrap());
-                let expiration_year = passport.get("eyr").map_or_else(|| 0, |p| p.parse::<i32>().unwrap());
-                let hair_colour = passport.get("hcl").unwrap_or(&empty);
-                let eye_colour = passport.get("ecl").unwrap_or(&empty);
-                let passport_id = passport.get("pid").unwrap_or(&empty);
-                let height = passport.get("hgt").unwrap_or(&zero).chars();
-
-                let (height_value, system): (String, String) = height.partition(|d| d.is_digit(10));
-                let height_value = height_value.parse::<i32>().unwrap();
-
-                if birth_year < 1920 || birth_year > 2002 {
-                    false
-                } else if issue_year < 2010 || issue_year > 2020 {
-                    false
-                } else if expiration_year < 2020 || expiration_year > 2030 {
-                    false
-                } else if hair_colour.chars().next().unwrap_or(' ') != '#' || hair_colour.len() < 7 || !hair_colour.chars().skip(1).all(|ch| ch.is_digit(16)) {
-                    false
-                } else if !vec!["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].iter().any(|colour| colour == eye_colour) {
-                    false
-                } else if passport_id.len() != 9 || passport_id.chars().any(|d| !d.is_digit(10)) {
-                    false
-                } else if (system != "cm" && system != "in") || (system == "cm" && (height_value < 150 || height_value > 193)) || (system == "in" && (height_value < 59 || height_value > 76)) {
-                    false
-                } else {
-                    true
-                }
-            }).count()
+            .filter_map(|passport| Passport::from(passport).ok())
+            .filter(&Passport::is_valid)
+            .count()
     }
 
     fn get_input() -> Self::Input {
@@ -88,5 +152,14 @@ impl Day for Four {
 
                 passports
             })
+            .into_iter()
+            .filter_map(|passport| {
+                if Passport::check(&passport) {
+                    Some(passport)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>()
     }
 }
