29c7ed61d8ad2af35b7efa794f5d094814aea4e6
Joshua Hill <josh@pyxxilated.studio>
2020-12-06 19:17:34 +1300

    Finish Day 6 Part 1/2

---
+++ src/day/six.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+
+use crate::day::Day;
+
+pub struct Six {}
+
+impl Day for Six {
+    type Input = Vec<(usize, HashMap<char, usize>)>;
+    type Output = usize;
+
+    fn part_one(answers: &Self::Input) -> Self::Output {
+        answers
+            .iter()
+            .fold(0, |count, (_, answer)| count + answer.keys().count())
+    }
+
+    fn part_two(answers: &Self::Input) -> Self::Output {
+        answers.iter().fold(0, |count, (c, answer)| {
+            count + answer.values().filter(|value| *value == c).count()
+        })
+    }
+
+    fn get_input() -> Self::Input {
+        let input = include_str!("input/day_six");
+
+        input
+            .lines()
+            .map(&str::trim)
+            .fold(vec![Vec::new()], |mut lines, line| {
+                if line.is_empty() {
+                    lines.push(Vec::new());
+                } else {
+                    lines.last_mut().unwrap().push(line.to_string());
+                }
+
+                lines
+            })
+            .iter()
+            .fold(Vec::new(), |mut answers, answer| {
+                answers.push((
+                    answer.len(),
+                    answer
+                        .into_iter()
+                        .fold(HashMap::new(), |mut values, value| {
+                            value.chars().for_each(|ch| {
+                                values.insert(ch, values.get(&ch).unwrap_or(&0) + 1);
+                            });
+                            values
+                        }),
+                ));
+
+                answers
+            })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use structopt::StructOpt;
 
 #[macro_use]
 mod day;
-use day::{five::Five, four::Four, one::One, three::Three, two::Two, Day};
+use day::{five::Five, four::Four, one::One, six::Six, three::Three, two::Two, Day};
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "calendar", about = "An Advent of Code Calendar")]
@@ -16,7 +16,7 @@ fn main() {
     let mut opt = Opt::from_args();
 
     if opt.days.is_empty() {
-        opt.days = vec![1, 2, 3, 4, 5];
+        opt.days = vec![1, 2, 3, 4, 5, 6];
     }
 
     opt.days.into_iter().for_each(|day| match day {
@@ -25,6 +25,7 @@ fn main() {
         3 => run!(Three),
         4 => run!(Four),
         5 => run!(Five),
+        6 => run!(Six),
         _ => eprintln!("That day doesn't exist in the calendar or hasn't passed yet!"),
     });
 }
